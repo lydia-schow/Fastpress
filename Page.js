636a29af3924fe536915ef08be73da20f20f527f
Elijah Schow <elijah.schow@gmail.com>
2017-03-06 16:08:47 -0600

    Refactor routing

--- Page.js
+++ Page.js
@@ -1,3 +1,5 @@
+var Promise = require('bluebird');
+var marked = require('marked');
 var mongoose = require('mongoose');
 
 var PageSchema = mongoose.Schema({
@@ -6,4 +8,46 @@ var PageSchema = mongoose.Schema({
   date: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model( 'Page', PageSchema );
+Page = mongoose.model( 'Page', PageSchema );
+
+exports.list = (reqest, response) => {
+  Page.find()
+  .then(pages => {
+    var output = 'Pages:\n';
+    pages.forEach(page => {
+      output += `<a href="/pages/${page._id}">page _id: ${page._id}</a>\n`;
+    });
+    response.send(output);
+  });
+};
+
+exports.create = (request, response) => {
+  Page.create({
+    title: request.body.title,
+    body: request.body.body
+  })
+  .then( page => {
+    console.log(`Created page ${page._id}`);
+    response.redirect(`/pages/${page._id}`);
+  });
+};
+
+exports.createView = (request, response) => {
+  response.render('page-create');
+};
+
+exports.view = (request, response) => {
+  let pageTitle;
+  Page.findById(request.params.pageId)
+  .then(page => {
+    pageTitle = page.title;
+    return Promise.promisify(marked)(page.body); // promisify then invoke
+  })
+  .then(html => {
+    response.render('page', {
+      title: pageTitle,
+      body: html
+    });
+  });
+  // TODO: 404 for non-existant pages
+};
---
+++ index.js
@@ -0,0 +1,35 @@
+var Promise = require('bluebird');
+var bodyParser = require('body-parser');
+var express = require('express');
+var path = require('path');
+
+var db = require('./database.js');
+var page = require('./page.js');
+
+var app = express();
+
+// Config
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use( (request, response, next) => {
+  console.log(`${request.method} ${request.path} ${JSON.stringify(request.params)}`);
+  next();
+});
+
+// Page
+app.get('/pages', page.list);
+app.get('/pages/create', page.createView);
+app.post('/pages/create', page.create);
+app.get('/pages/:pageId',  page.view);
+
+// Site (TODO)
+
+// User (TODO)
+
+// Server
+const port = process.env.PORT || 3000;
+const success = () => { console.log(`Listening at port ${port}`); };
+app.listen( port, success );
