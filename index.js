1e93b168a20a00ef8d15aed114ff020c7310447b
Elijah Schow <elijah.schow@gmail.com>
2020-01-19 13:59:47 -0600

    Switch to .env config and re-oganize index.js

--- index.js
+++ index.js
@@ -5,38 +5,35 @@ const path = require('path');
 const session = require('express-session');
 const helmet = require('helmet');
 const MongoStore = require('connect-mongo')(session);
-
-const db = require('./database');
-const error = require('./error');
-const env = require('./env/environment');
-const doc = require('./controllers/document');
+require('dotenv').config();
 
 const app = express();
 
-// Improve security
-app.use(helmet());
-
-// Config
+// Set view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+// Middleware
+app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({
   resave: false, // don't save session if unmodified
   saveUninitialized: true, // always create a session
-  secret: env.sessionSecret,
-  store: new MongoStore({ mongooseConnection: db })
+  secret: process.env.SESSION_SECRET,
+  store: new MongoStore({ mongooseConnection: require('./database') })
 }));
-
-// Static files
 app.use('/', express.static('public'));
 
-// General
+/** Routes **/
+
+// Index
 router.get('/', (request, response) => {
   response.render('index');
 });
 
 // Document
+const doc = require('./controllers/document');
 router.get('/documents/', doc.list);
 router.route('/documents/create')
   .get(doc.createView)
@@ -47,13 +44,13 @@ router.route('/documents/create')
   .post(doc.edit);
 
 // Errors
+const error = require('./error');
 router.use(error.handle);
 
 app.use(router);
 
-// Start Server
+/** Start Server **/
 const port = process.env.PORT || 3000;
 const success = () => { console.log(`Listening at port ${port}`); };
-app.listen( port, success );
+app.listen(port, success);
 
-// Generic Error Handling (TODO)
